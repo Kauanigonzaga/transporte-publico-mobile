177ec7a2c6171ae6695ba6c29dd53e60526999ec
dudagasparotto <dudagasparotto05@gmail.com>
2026-03-19 20:25:50 -0300

    a

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import HomeMotorista from './src/telas/homeMotorista';
+/*import HomeMotorista from './src/telas/homeMotorista';*/
+import Rotas from './src/telas/rotas';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeMotorista />
+      <Rotas />
       <StatusBar style="auto" />
     </View>
   );
