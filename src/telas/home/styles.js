b058edc40fb257a7429c11cc0e6664cf3f257ee4
Joaogabriel6 <jgabriel2905@gmail.com>
2026-03-20 20:37:45 -0300

    20/03/2026

---
+++ src/telas/home/styles.js
@@ -0,0 +1,38 @@
+const styles = {
+
+    container: {
+        flex: 1,
+        alingnItems: 'center',
+        justifyContent: 'center',
+        color: '#1582e9',
+    },
+    titulo: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    botaoRoxo: {
+        backgroundColor: '#8e82c2',
+        alingnItems: 'center',
+        marginBottom: 10,
+       
+    },
+    botaoAzul: {
+        backgroundColor: '#3c8ae4',
+        alingnItems: 'center',
+        marginBottom: 10,
+    },
+    botaoAmarelo:{
+        backgroundColor: '#edda0e',
+        alingnItems: 'center',
+        marginBottom: 10,
+    },
+    botaoLaranja:{
+        backgroundColor: '#dc850b',
+        alingnItems: 'center',
+        marginBottom: 10,
+    },
+};
+
+
+export default styles;
