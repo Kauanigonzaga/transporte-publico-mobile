4f1252f3adc8315b353c471803ddb1f63d782c38
MARIA EDUARDA SILVA GASPAROTTO <maria.gasparotto>
2026-03-20 19:28:52 -0300

    a

--- App.js
+++ App.js
@@ -1,29 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-<<<<<<< HEAD
+
 /*import HomeMotorista from './src/telas/homeMotorista';*/
 import Rotas from './src/telas/rotas';
-=======
-<<<<<<< Updated upstream
+
 import HomeMotorista from './src/telas/homeMotorista';
-=======
+
 import Login from "./src/telas/login";
->>>>>>> Stashed changes
->>>>>>> 8ae9ac27273e4a8a1fb3bbc00ad2e429513e0265
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
+
       <Rotas />
-=======
-<<<<<<< Updated upstream
       <HomeMotorista />
-=======
       <Login />
->>>>>>> Stashed changes
->>>>>>> 8ae9ac27273e4a8a1fb3bbc00ad2e429513e0265
+
       <StatusBar style="auto" />
     </View>
   );
