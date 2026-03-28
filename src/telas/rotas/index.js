20d0ef9229fa246a3e98af1834c868cacbda648c
dudagasparotto <dudagasparotto05@gmail.com>
2026-03-27 21:31:24 -0300

    BOTAO HOME

--- src/telas/rotas/index.js
+++ src/telas/rotas/index.js
@@ -1,41 +1,35 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, TouchableOpacity,ImageBackground } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './styles.js';
-import  Image from '../../../assets/imgMotorista.png';
+import image from '../../../assets/imgMotorista.png';
 // import MapView from 'react-native-maps';
 
 export default function RotasScreen() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
   return (
-
-    <ImageBackground source={ Image } style={styles.logo}> 
-    
     <SafeAreaView style={styles.screen}>
 
-        {/*<Stack.Navigator>
+      {/*<Stack.Navigator>
             <Stack.Screen name="Pontos" component={pontos} />
         </Stack.Navigator>*/}
 
       <View style={styles.panel}>
         <View style={styles.topBar}>
           <View style={styles.motoristaBox}>
-            <Text style={styles.motoristaText}>MOTORISTA</Text>
+
+            <TouchableOpacity style={styles.menuButton} onPress={() => navigation.navigate('avaliacaoMotorista')}>
+            <Text style={styles.menuButtonText}>MOTORISTA</Text>
+            </TouchableOpacity>
+            
+            <Image source={image} style={styles.logo} />
           </View>
 
           <View style={styles.menuBox}>
-            <TouchableOpacity style={styles.menuButton }>
-              <Text style={styles.menuButtonText}>HORÁRIOS</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.menuButton} onPress={() => navigation.navigate('Linhas')}>
-              <Text style={styles.menuButtonText}>LINHAS</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.menuButton} onPress={() => navigation.navigate('Pontos')}>
-              <Text style={styles.menuButtonText}>PONTOS</Text>
+            <TouchableOpacity style={styles.menuButton} onPress={() => navigation.navigate('Home')}>
+              <Text style={styles.menuButtonText}>HOME</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -52,8 +46,8 @@ export default function RotasScreen() {
 
         <View style={styles.bottomBar} />
       </View>
-      </SafeAreaView>
-     </ImageBackground> 
+    </SafeAreaView>
+
   );
 }
 
